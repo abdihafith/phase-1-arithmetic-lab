5450d4cb12f3163543910ec98a5095f44370a483
Abdihafith <abdyhafith@gmail.com>
2024-03-24 20:25:39 +0300

    complete

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-
 // Write your code here
+// Write your code here
+
+
+let num1 = 31, num2 = 2;
+const multiply = 31 * 2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 22, num4 = 9;
+const mod = 22 % 9;
+
+let max = Math.max(1,20,3,10);
